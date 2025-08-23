255a1aa496af128ccf19b79309b4ee00a17b8157
himajavankayala0129 <himudotnet@gmail.com>
2025-08-22 23:27:04 -0400

    Update __init__.py

--- syda/__init__.py
+++ syda/__init__.py
@@ -14,8 +14,8 @@ __all__ = [
     'ProxyConfig'
 ]
 
-__version__ = '0.0.1'
-__author__ = 'Syda AI Team'
-__email__ = 'contact@syda-ai.org'
+__version__ = '0.0.2'
+__author__ = 'Rama Krishna Kumar Lingamgunta'
+__email__ = 'lrkkumar2606@gmail.com'
 __license__ = 'LGPL-3.0-or-later'
 __description__ = 'A Python library for AI-powered synthetic data generation with referential integrity'
